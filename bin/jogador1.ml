5003a0b2752c0c96116b0fc138440529071f9371
Henrique <henrique.laia@ubi.pt>
2025-05-15 18:44:14 +0100

    Atualizei jogador1.ml para alternar os jogadores(X ou O), melhorei validação das jogadas em help.ml(pois estava a deixar colocar mesmo com a coluna ja cheia) e board.ml, e corrigi dependências em bin/dune

--- bin/jogador1.ml
+++ bin/jogador1.ml
@@ -2,18 +2,18 @@ open Connect_lib.Board
 open Connect_lib.Help
 
 let () =
-  (* Inicializa o tabuleiro *)
-  let rec loop tabuleiro jogada_atual =
-    Printf.printf "Jogada número %d\n" jogada_atual;
-    let tabuleiro_novo = pedir_jogada tabuleiro 'X' in
+  
+  let rec loop tabuleiro jogador jogada_atual =
+    Printf.printf "Jogada número %d - Jogador %c\n" jogada_atual jogador;
+    let tabuleiro_novo = pedir_jogada tabuleiro jogador in
     print tabuleiro_novo;
 
-    (* Verifica se o jogo terminou *)
-    if fim_de_jogo tabuleiro_novo 'X' then
-      Printf.printf "Jogador X venceu!\n"
+    if fim_de_jogo tabuleiro_novo jogador then
+      Printf.printf "Jogador %c venceu!\n" jogador
     else (
-      loop tabuleiro_novo (jogada_atual + 1)
+      let proximo_jogador = if jogador = 'X' then 'O' else 'X' in
+      loop tabuleiro_novo proximo_jogador (jogada_atual + 1)
     )
   in
 
-  loop (tabuleiro_vazio ()) 1
+  loop (tabuleiro_vazio ()) 'X' 1
--- lib/help.ml
+++ lib/help.ml
@@ -1,6 +1,6 @@
 open Board
 
-let pedir_jogada (t : tabuleiro) (peca : celula) : tabuleiro =
+let rec pedir_jogada (t : tabuleiro) (peca : celula) : tabuleiro =
   Printf.printf "Escolha a coluna onde quer jogar (0-6): ";
   let entrada = read_line () in
   try
@@ -8,12 +8,12 @@ let pedir_jogada (t : tabuleiro) (peca : celula) : tabuleiro =
     if jogada_valida t coluna then
       let t = aplicar_jogada t coluna peca in
       print t;
-      t (* devolve o tabuleiro atualizado *)
+      t
     else (
-      Printf.printf "Coluna inválida!\n";
-      t (* devolve o tabuleiro original *)
+      Printf.printf "Coluna inválida ou cheia!\n";
+      pedir_jogada t peca
     )
   with
   | Failure _ ->
       Printf.printf "Entrada inválida! Por favor, insira um número.\n";
-      t (* devolve tabuleiro original *)
+      pedir_jogada t peca
